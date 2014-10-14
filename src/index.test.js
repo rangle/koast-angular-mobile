b304735d2a32f1c16f3f8a2d5c3a5173d6413d1a
Evan <evan@rangle.io>
2014-10-14 14:14:45 -0400

    [#80669090] chore (setup) initial setup for package and build

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+'use strict';
+var expect = chai.expect;
+
+describe('the koast angular mobile app', function () {
+  it('should blow up', function () {
+    expect(1).to.be.equal(2);
+  });
+});
---
+++ src/sections/notification/notification.test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = chai.expect;
+
+describe('mobile notifications', function () {
+  it('should blow up', function () {
+    expect(1).to.be.equal(2);
+  });
+
+});
